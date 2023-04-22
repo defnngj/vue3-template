feb5206d63085d9b12b7c88f7823259b310dcd9e
defnngj <defnngj@gmail.com>
2023-04-22 17:40:29 +0800

    feat: vue3 template

---
+++ src/views/AboutView.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+// do not use same name with ref
+
+import { ref } from 'vue'
+
+const number = ref(1)
+
+const data = reactive({
+  input:1
+})
+
+const onSubmit = () => {
+  number.value ++
+  data.input ++
+  console.log('submit!', number.value, data.input)
+}
+
+</script>
+
+<template>
+  <div class="about">
+    <div>
+      <h1>This is an about page! {{ number }}</h1>
+    </div>
+    <div>
+      <!-- <el-input v-model="data.input" /> -->
+      <el-button type="primary" @click="onSubmit"> + 1</el-button>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
